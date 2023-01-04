999f24640e8fcc7e4468a67b5358f851ab825bc3
DEADPOOL712 <98164944+DEADPOOL712@users.noreply.github.com>
2023-01-04 18:26:54 +0530

    b

---
+++ tower_of_hanoi.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+// n - number of disk
+// a - b - c
+// 1. move n-1 disk from a to b using c
+// 2. print moved last disk from a to c
+// 3. move n-1 disk from b to c using a
+void toh(int n, char a, char b, char c)
+{
+    if (n > 0)
+    {
+
+        toh(n - 1, a, c, b);
+        printf("%d moved from %c to %c \n", n, a, c);
+        toh(n - 1, b, a, c);
+    }
+}
+int main()
+{
+    toh(3, 'A', 'B', 'C');
+    return 0;
+}
